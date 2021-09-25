eb62697497dad40694d0c4b5442435a8657451c7
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2021-09-25 14:44:58 +0100

    Add support for nginx without a TLS reverse proxy.

--- configure-instance.sh
+++ configure-instance.sh
@@ -7,7 +7,11 @@ read backend
 echo "Enter a hostname for the Proxy (eg: pipedproxy.kavin.rocks):"
 read proxy
 
+echo "Enter the reverse proxy you would like to use (either caddy or nginx):"
+read reverseproxy
+
 rm -rf config/
+rm -f docker-compose.yml
 
 cp -r template/ config/
 
@@ -15,4 +19,4 @@ sed -i "s/FRONTEND_HOSTNAME/$frontend/g" config/*
 sed -i "s/BACKEND_HOSTNAME/$backend/g" config/*
 sed -i "s/PROXY_HOSTNAME/$proxy/g" config/*
 
-mv config/docker-compose.yml docker-compose.yml
+mv config/docker-compose.$reverseproxy.yml docker-compose.yml
