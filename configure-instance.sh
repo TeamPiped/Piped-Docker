7f8e11e66d60e64fa361e7b5db14eebd841ef23a
Andrea Spacca <andrea.spacca@gmail.com>
2021-11-17 09:32:16 +0100

    support full urls

--- configure-instance.sh
+++ configure-instance.sh
@@ -1,11 +1,11 @@
-echo "Enter a hostname for the Frontend (eg: piped.kavin.rocks):"
-read frontend
+echo "Enter an url for the Frontend (eg: https://piped.kavin.rocks):"
+read frontend_url
 
-echo "Enter a hostname for the Backend (eg: pipedapi.kavin.rocks):"
-read backend
+echo "Enter an url for the Backend (eg: https://pipedapi.kavin.rocks):"
+read backend_url
 
-echo "Enter a hostname for the Proxy (eg: pipedproxy.kavin.rocks):"
-read proxy
+echo "Enter an hostname for the Proxy (eg: https://pipedproxy.kavin.rocks):"
+read proxy_url
 
 echo "Enter the reverse proxy you would like to use (either caddy or nginx):"
 read reverseproxy
@@ -15,8 +15,16 @@ rm -f docker-compose.yml
 
 cp -r template/ config/
 
-sed -i "s/FRONTEND_HOSTNAME/$frontend/g" config/*
-sed -i "s/BACKEND_HOSTNAME/$backend/g" config/*
-sed -i "s/PROXY_HOSTNAME/$proxy/g" config/*
+frontend_hostname=$(echo "$frontend_url"| awk -F[/:] '{print $4}')
+backend_hostname=$(echo "$backend_url"| awk -F[/:] '{print $4}')
+proxy_hostname=$(echo "$proxy_url"| awk -F[/:] '{print $4}')
+
+sed -i "s@FRONT_URL@$frontend_url@g" config/*
+sed -i "s@BACKEND_URL@$backend_url@g" config/*
+sed -i "s@PROXY_URL@$proxy_url@g" config/*
+
+sed -i "s@FRONTEND_HOSTNAME@$frontend_hostname@g" config/*
+sed -i "s@BACKEND_HOSTNAME@$backend_hostname@g" config/*
+sed -i "s@PROXY_HOSTNAME@$proxy_hostname@g" config/*
 
 mv config/docker-compose.$reverseproxy.yml docker-compose.yml
