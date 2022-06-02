aa3f174f5e171234243c6b1afad960ca865e064a
Kavin <20838718+FireMasterK@users.noreply.github.com>
2022-06-02 04:38:55 +0100

    Remove docker-compose version checks.

--- configure-instance.sh
+++ configure-instance.sh
@@ -4,15 +4,6 @@ if [ -z "$(which docker-compose)" ]; then
     exit 1
 fi
 
-# Docker-Compose version check, to prevent "Unsupported configuration option"
-COMPOSE_VERSION=$(docker-compose version --short 2>/dev/null || docker compose version --short 2>/dev/null)
-REQUIRED_COMPOSE_VERSION="1.28.0"
-if [[ $(printf '%s\n' "$REQUIRED_COMPOSE_VERSION" "$COMPOSE_VERSION" | sort -V | head -n1) != $REQUIRED_COMPOSE_VERSION ]]; then
-    echo "Your docker-compose version of $COMPOSE_VERSION is too old. Please upgrade to $REQUIRED_COMPOSE_VERSION or higher."
-    echo "See https://docs.docker.com/compose/install/#install-compose for installation instructions."
-    exit 1
-fi
-
 echo "Enter a hostname for the Frontend (eg: piped.kavin.rocks):" && read -r frontend
 echo "Enter a hostname for the Backend (eg: pipedapi.kavin.rocks):" && read -r backend
 echo "Enter a hostname for the Proxy (eg: pipedproxy.kavin.rocks):" && read -r proxy
