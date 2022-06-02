92c41f24652d02a9ca197d72030f24be6b8fc1c1
Kavin <20838718+FireMasterK@users.noreply.github.com>
2022-06-02 04:38:55 +0100

    Remove docker-compose version checks.

--- configure-instance.sh
+++ configure-instance.sh
@@ -1,24 +1,6 @@
-# Check if Docker-Compose is not installed
-if [ -z "$(which docker-compose)" ]; then
-    echo "Docker-Compose is not installed. Please install it first from https://docs.docker.com/compose/install/#install-compose."
-    exit 1
-fi
-
-# Docker-Compose version check, to prevent "Unsupported configuration option"
-COMPOSE_VERSION=$(docker-compose version --short 2>/dev/null || docker compose version --short 2>/dev/null)
-REQUIRED_COMPOSE_VERSION="1.28.0"
-if [[ $(printf '%s\n' "$REQUIRED_COMPOSE_VERSION" "$COMPOSE_VERSION" | sort -V | head -n1) != $REQUIRED_COMPOSE_VERSION ]]; then
-    echo "Your docker-compose version of $COMPOSE_VERSION is too old. Please upgrade to $REQUIRED_COMPOSE_VERSION or higher."
-    echo "See https://docs.docker.com/compose/install/#install-compose for installation instructions."
-    exit 1
-fi
-
-echo "Enter an url for the Frontend (eg: https://piped.kavin.rocks):" && read -r frontend_url
-
-echo "Enter an url for the Backend (eg: https://pipedapi.kavin.rocks):" && read -r backend_url
-
-echo "Enter an hostname for the Proxy (eg: https://pipedproxy.kavin.rocks):" && read -r proxy_url
-
+echo "Enter an url for the Frontend (eg: https://piped.kavin.rocks):" && read -r frontend
+echo "Enter an url for the Backend (eg: https://pipedapi.kavin.rocks):" && read -r backend
+echo "Enter an hostname for the Proxy (eg: https://pipedproxy.kavin.rocks):" && read -r proxy
 echo "Enter the reverse proxy you would like to use (either caddy or nginx):" && read -r reverseproxy
 
 rm -rf config/
@@ -26,9 +8,17 @@ rm -f docker-compose.yml
 
 cp -r template/ config/
 
+frontend_schema=$(echo "$frontend_url"| awk -F[/:] '{print $1}')
+backend_schema=$(echo "$backend_url"| awk -F[/:] '{print $1}')
+proxy_schema=$(echo "$proxy_url"| awk -F[/:] '{print $1}')
+
 frontend_hostname=$(echo "$frontend_url"| awk -F[/:] '{print $4}')
 backend_hostname=$(echo "$backend_url"| awk -F[/:] '{print $4}')
 proxy_hostname=$(echo "$proxy_url"| awk -F[/:] '{print $4}')
+
+frontend_port=$(echo "$frontend_url"| awk -F[/:] '{print $5}'| { read -d '' port; [ -z "$port" ] && { [ "$frontend_schema" = "https" ] && echo "443" || echo "80"; } || echo "$port"; })
+backend_port=$(echo "$backend_url"| awk -F[/:] '{print $5}'| { read -d '' port; [ -z "$port" ] && { [ "$backend_schema" = "https" ] && echo "443" || echo "80"; } || echo "$port"; })
+proxy_port=$(echo "$proxy_url"| awk -F[/:] '{print $5}'| { read -d '' port; [ -z "$port" ] && { [ "$proxy_schema" = "https" ] && echo "443" || echo "80"; } || echo "$port"; })
 
 sed -i "s@FRONT_URL@$frontend_url@g" config/*
 sed -i "s@BACKEND_URL@$backend_url@g" config/*
@@ -37,6 +27,10 @@ sed -i "s@PROXY_URL@$proxy_url@g" config/*
 sed -i "s@FRONTEND_HOSTNAME@$frontend_hostname@g" config/*
 sed -i "s@BACKEND_HOSTNAME@$backend_hostname@g" config/*
 sed -i "s@PROXY_HOSTNAME@$proxy_hostname@g" config/*
+
+sed -i "s@FRONTEND_PORT@$frontend_port@g" config/*
+sed -i "s@BACKEND_PORT@$backend_port@g" config/*
+sed -i "s@PROXY_PORT@$proxy_port@g" config/*
 
 # The openj9 image does not support aarch64
 if [[ "$(uname -m)" == "aarch64" ]]; then
