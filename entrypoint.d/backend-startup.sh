c8ef39e3cbdac36c118b34163a679c49f4e9f7a3
Bento Silveira <bento@ninjazumbi.com>
2023-07-23 12:44:00 -0300

    Added the script that set the database before starting the backend

---
+++ entrypoint.d/backend-startup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "hibernate.connection.url: jdbc:postgresql://${POSTGRES_DB}:5432/piped" > /app/config.properties
+echo "hibernate.connection.username: ${POSTGRES_USER}" >> /app/config.properties
+echo "hibernate.connection.password: ${POSTGRES_PASSWORD}" >> /app/config.properties
+echo "hibernate.connection.driver_class: org.postgresql.Driver" >> /app/config.properties
+echo "hibernate.dialect: org.hibernate.dialect.PostgreSQLDialect" >> /app/config.properties
+
+exec java -server -Xmx1G -XX:+UnlockExperimentalVMOptions -XX:+OptimizeStringConcat -XX:+UseStringDeduplication -XX:+UseCompressedOops -XX:+UseNUMA -XX:+UseG1GC -Xshare:on -jar /app/piped.jar
