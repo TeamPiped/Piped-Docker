d3a3225ea75f2be0d57b48fc21d1611b03cfae3e
Benjamin Salzberg <besrasal@gmail.com>
2024-04-28 07:59:35 +0000

    Add handling of custom ports (might not work for caddy)

--- configure-instance.sh
+++ configure-instance.sh
@@ -3,6 +3,7 @@ echo "Enter a hostname for the Frontend (eg: piped.kavin.rocks):" && read -r fro
 echo "Enter a hostname for the Backend (eg: pipedapi.kavin.rocks):" && read -r backend
 echo "Enter a hostname for the Proxy (eg: pipedproxy.kavin.rocks):" && read -r proxy
 echo "Enter the reverse proxy you would like to use (either caddy or nginx):" && read -r reverseproxy
+echo "Enter a port to use for the reverse proxy:" && read -r port
 
 rm -rf config/
 rm -f docker-compose.yml
@@ -12,5 +13,6 @@ cp -r template/ config/
 sed -i "s/FRONTEND_HOSTNAME/$frontend/g" config/*
 sed -i "s/BACKEND_HOSTNAME/$backend/g" config/*
 sed -i "s/PROXY_HOSTNAME/$proxy/g" config/*
+sed -i "s/PORT_VALUE/$port/g" config/*
 
 mv config/docker-compose.$reverseproxy.yml docker-compose.yml
