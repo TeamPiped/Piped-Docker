531c677c9831420c045e8270e97d752ae5b3d851
Bnyro <bnyro@tutanota.com>
2025-01-07 21:03:08 +0100

    feat: support for hosting only via http using configure-instance.sh

--- configure-instance.sh
+++ configure-instance.sh
@@ -3,6 +3,7 @@ echo "Enter a hostname for the Frontend (eg: piped.kavin.rocks):" && read -r fro
 echo "Enter a hostname for the Backend (eg: pipedapi.kavin.rocks):" && read -r backend
 echo "Enter a hostname for the Proxy (eg: pipedproxy.kavin.rocks):" && read -r proxy
 echo "Enter the reverse proxy you would like to use (either caddy or nginx):" && read -r reverseproxy
+echo "Is your hostname reachable via HTTP or HTTPS? (eg: https)" && read -r http_mode
 
 rm -rf config/
 rm -f docker-compose.yml
@@ -15,5 +16,6 @@ sed -i "s/BACKEND_HOSTNAME/$backend/g" $conffiles
 sed -i "s/PROXY_HOSTNAME/$proxy/g" $conffiles
 
 sed -i "s/BACKEND_HOSTNAME_PLACEHOLDER/$backend/g" config/*.yml
+sed -i "s/HTTP_MODE_PLACEHOLDER/$http_mode/g" config/*.yml
 
 mv config/docker-compose.$reverseproxy.yml docker-compose.yml
