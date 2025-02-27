87e1239d9db18222ed4e817af46048b19522fd7a
Bnyro <bnyro@tutanota.com>
2025-02-27 12:10:54 +0100

    feat: add stanalone installation process

--- configure-instance.sh
+++ configure-instance.sh
@@ -2,7 +2,7 @@
 echo "Enter a hostname for the Frontend (eg: piped.kavin.rocks):" && read -r frontend
 echo "Enter a hostname for the Backend (eg: pipedapi.kavin.rocks):" && read -r backend
 echo "Enter a hostname for the Proxy (eg: pipedproxy.kavin.rocks):" && read -r proxy
-echo "Enter the reverse proxy you would like to use (either caddy or nginx):" && read -r reverseproxy
+echo "Enter the reverse proxy you would like to use (caddy, nginx or standalone):" && read -r reverseproxy
 echo "Is your hostname reachable via HTTP or HTTPS? (eg: https)" && read -r http_mode
 
 rm -rf config/
